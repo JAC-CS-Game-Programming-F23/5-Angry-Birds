496886e3768e7263c35b4c30f92f5d3d913c6346
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2023-10-22 20:19:32 -0400

    Initial commit.

---
+++ src/Angry-Birds-1/src/globals.js
@@ -0,0 +1,34 @@
+import StateMachine from "../lib/StateMachine.js";
+
+export const canvas = document.createElement('canvas');
+export const context = canvas.getContext('2d') || new CanvasRenderingContext2D();
+export const CANVAS_WIDTH = 2000;
+export const CANVAS_HEIGHT = 720;
+
+export const keys = {};
+export const stateMachine = new StateMachine();
+
+/**
+ * The physics engine we're going to use for this game.
+ * We're not importing it anywhere because it is declared globally in index.html.
+ *
+ * @see https://brm.io/matter-js
+ */
+// @ts-ignore
+export const matter = Matter;
+
+/**
+ * The Matter.Engine module contains methods for creating and manipulating engines.
+ * An engine is a controller that manages updating the simulation of the world.
+ *
+ * @see https://brm.io/matter-js/docs/classes/Engine.html
+ */
+export const engine = matter.Engine.create();
+
+/**
+ * The root Matter.Composite instance that will contain all bodies,
+ * constraints and other composites to be simulated by this engine.
+ *
+ * @see https://brm.io/matter-js/docs/classes/Engine.html#property_world
+ */
+export const world = engine.world;
